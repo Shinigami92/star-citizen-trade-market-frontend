4e4d5647470bcb61fb9679f8fe470f3ccaa2699b
Christopher Quadflieg <chrissi92@hotmail.de>
2018-12-06 23:13:40 +0100

    queried commodities styled table

--- src/views/item/item.ts
+++ src/views/item/item.ts
@@ -1,4 +1,21 @@
+import gql from 'graphql-tag';
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component
+@Component({
+	apollo: {
+		commodities: gql`
+			query commodities {
+				commodities {
+					id
+					name
+					... on Commodity {
+						commodityCategory {
+							name
+						}
+					}
+				}
+			}
+		`
+	}
+})
 export default class ItemDashboard extends Vue {}
--- src/views/item/item.vue
+++ src/views/item/item.vue
@@ -1,3 +1,4 @@
 <template src="./item.html"></template>
 <script lang="ts" src="./item.ts">
 </script>
+<style lang="scss" scoped src="./item.scss"></style>
