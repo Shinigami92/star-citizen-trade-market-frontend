8fccf5e8b45941376db887a3e5386b76d0db8924
Christopher Quadflieg <chrissi92@hotmail.de>
2019-05-27 20:20:43 +0200

    update dependencies

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,4 @@
 declare module '*.vue' {
-	import ApolloClient from 'apollo-client';
 	import Vue from 'vue';
-	module 'vue/types/vue' {
-		interface Vue {
-			$apolloProvider: { defaultClient: ApolloClient<{}> };
-		}
-	}
 	export default Vue;
 }
