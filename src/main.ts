46fc1b09f9c47b4ce7b1821b6481d91fb69dd8fe
Christopher Quadflieg <chrissi92@hotmail.de>
2019-06-12 19:52:49 +0200

    Rewrite apollo provider

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Vue, { CreateElement, VNode } from 'vue';
 import App from './app.vue';
-import { createProvider } from './plugins/apollo';
+import { apolloProvider } from './plugins/apollo';
 import './plugins/moment';
 import './plugins/vuetify';
 import router from './router';
@@ -9,7 +9,7 @@ import router from './router';
 Vue.config.productionTip = false;
 
 new Vue({
-	apolloProvider: createProvider(),
+	apolloProvider,
 	render: (h: CreateElement): VNode => h(App),
 	router
 }).$mount('#app');
