10b7be665e9ba072eb9382d6f4d3a1c0e79ea131
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2019-10-28 13:36:33 +0100

    Update limits

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef { import("@vue/cli-service").ProjectOptions } Options
+ */
+
+/**
+ * @type { Options }
+ */
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+	chainWebpack(config) {
+		config.performance.maxAssetSize(2_097_152 /* 2 MiB */).maxEntrypointSize(5_242_880 /* 5 MiB */);
+	}
 };
