c49a18ce24196dec5dd84752eb11c270ce328fad
Christopher Quadflieg <chrissi92@hotmail.de>
2019-12-15 14:18:24 +0100

    Remove number seperators

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,6 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 	chainWebpack(config) {
-		config.performance.maxAssetSize(2_097_152 /* 2 MiB */).maxEntrypointSize(5_242_880 /* 5 MiB */);
+		config.performance.maxAssetSize(2097152 /* 2 MiB */).maxEntrypointSize(5242880 /* 5 MiB */);
 	}
 };
