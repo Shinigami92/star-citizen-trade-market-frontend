078f0144090178c6ed9756ea24b03eeeb708e6fa
Christopher Quadflieg <chrissi92@hotmail.de>
2020-01-25 20:05:00 +0100

    Improve error handling

--- src/views/sign-up/sign-up.ts
+++ src/views/sign-up/sign-up.ts
@@ -7,11 +7,15 @@ import isEmail from 'validator/lib/isEmail';
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
+function checkErrorMessage(message: string | null, expectedMessage: string): string | true {
+  return !message || message !== expectedMessage || message;
+}
+
 @Component
 export default class SignUp extends Vue {
-  public valid: boolean = false;
+  // Error message must be outside the component, otherwise rules cannot find it
+  public static errorMessage: string | null = null;
   public errors: GraphQLError[] | null = null;
-  public errorMessage: any | null = null;
 
   public username: string = '';
   public handle: string = '';
@@ -20,15 +24,18 @@ export default class SignUp extends Vue {
 
   public readonly usernameRules: ValidationRule[] = [
     (v) => !!v || 'Username is required',
-    (v) => v.length >= 3 || 'Username must be greater than or equals 3 characters'
+    (v) => v.length >= 3 || 'Username must be greater than or equals 3 characters',
+    (v) => checkErrorMessage(SignUp.errorMessage, `Username ${v} is already in use`)
   ];
   public readonly handleRules: ValidationRule[] = [
     (v) => !!v || 'Handle is required',
-    (v) => v.length <= 50 || 'Handle must be less than 50 characters'
+    (v) => v.length <= 50 || 'Handle must be less than 50 characters',
+    (v) => checkErrorMessage(SignUp.errorMessage, `Star Citizen Handle ${v} is already taken by another user`)
   ];
   public readonly emailRules: ValidationRule[] = [
     (v) => !!v || 'E-Mail is required',
-    (v) => isEmail(v) || 'E-Mail must be valid'
+    (v) => isEmail(v) || 'E-Mail must be valid',
+    (v) => checkErrorMessage(SignUp.errorMessage, `Email ${v} is already in use`)
   ];
   public readonly confirmEmailRules: ValidationRule[] = [
     (v) => !!v || 'Confirm E-Mail is required',
@@ -39,10 +46,19 @@ export default class SignUp extends Vue {
     super();
   }
 
+  public get invalid(): boolean {
+    return (
+      this.usernameRules.some((rule) => rule(this.username) !== true) ||
+      this.handleRules.some((rule) => rule(this.handle) !== true) ||
+      this.emailRules.some((rule) => rule(this.email) !== true) ||
+      this.confirmEmailRules.some((rule) => rule(this.confirmEmail) !== true)
+    );
+  }
+
   public async submit(): Promise<void> {
     try {
       this.errors = null;
-      this.errorMessage = null;
+      SignUp.errorMessage = null;
       const result: FetchResult<any, Record<string, any>, Record<string, any>> = await this.$apollo.mutate({
         mutation: gql`
           mutation SignUp($input: CreateAccountInput!) {
@@ -65,7 +81,8 @@ export default class SignUp extends Vue {
     } catch (error) {
       console.error(error.graphQLErrors);
       this.errors = error.graphQLErrors as GraphQLError[];
-      this.errorMessage = JSON.parse(this.errors[0].message);
+      SignUp.errorMessage = JSON.parse(JSON.stringify(this.errors[0].message)).message;
+      (this.$refs.form as any).validate();
     }
   }
 }
